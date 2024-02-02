d5a8d06268272f0c155d090dae01210ab3f4742d
Mayya Matueva <mayamatueva@gmail.com>
2024-02-02 13:27:24 +0100

    add functionality

---
+++ app.js
@@ -0,0 +1,27 @@
+const todoForm = document.querySelector("#todo-form");
+const todoList = document.querySelector(".todos");
+const totalTasks = document.querySelector("#total-tasks");
+const completedTasks = document.querySelector("#completed-tasks");
+const remainingTasks = document.querySelector("#remaining-tasks");
+const mainInput = document.querySelector("#todo-form input");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputValue = mainInput.value;
+
+  if (inputValue === "") {
+    return;
+  }
+
+  const task = {
+    id: new Date().getTime(),
+    name: inputValue,
+    isCompleted: false,
+  };
+
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+});
